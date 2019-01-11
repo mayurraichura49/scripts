6d3b0f3b6da46f1206be0bda184583728ca39022
mayurraichura49 <40169422+mayurraichura49@users.noreply.github.com>
2019-01-11 15:04:03 +0530

    Create xls_write_basic.pl
    This is a basic script to write any data in xls through perl.

---
+++ xls_write_basic.pl
@@ -0,0 +1,22 @@
+use Spreadsheet::WriteExcel;
+ 
+# Create a new Excel workbook
+my $workbook = Spreadsheet::WriteExcel->new('perl.xls');
+ 
+# Add a worksheet
+$worksheet = $workbook->add_worksheet();
+ 
+#  Add and define a format
+$format = $workbook->add_format(); # Add a format
+$format->set_bold();
+$format->set_color('red');
+$format->set_align('center');
+ 
+# Write a formatted and unformatted string, row and column notation.
+$col = $row = 0;
+$worksheet->write($row, $col, 'Hi Excel!', $format);
+$worksheet->write(1,    $col, 'Hi Excel!');
+ 
+# Write a number and a formula using A1 notation
+$worksheet->write('A3', 1.2345);
+$worksheet->write('A4', '=SIN(PI()/4)');
